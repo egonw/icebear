af4c3ed9ccf757c552b7636c464f701eb3acc2d0
Egon Willighagen <egon.willighagen@gmail.com>
2021-07-03 20:22:49 +0200

    First implementation

--- src/main/java/net/bioclipse/manager/IcebearManager.java
+++ src/main/java/net/bioclipse/manager/IcebearManager.java
@@ -9,10 +9,23 @@
  */
 package net.bioclipse.manager;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import io.github.egonw.bacting.IBactingManager;
+import net.bioclipse.cdk.domain.ICDKMolecule;
+import net.bioclipse.core.business.BioclipseException;
+import net.bioclipse.core.domain.IMolecule;
+import net.bioclipse.core.domain.IMolecule.Property;
+import net.bioclipse.managers.CDKManager;
+import net.bioclipse.managers.RDFManager;
+import net.bioclipse.rdf.business.IRDFStore;
 
 /**
  * Manager for crawling chemical information on the internet. 
@@ -20,6 +33,15 @@ import io.github.egonw.bacting.IBactingManager;
 public class IcebearManager implements IBactingManager {
 
 	private String workspaceRoot;
+	private CDKManager cdk;
+	private RDFManager rdf;
+
+	Map<String,String> extraHeaders = new HashMap<String, String>() {
+		private static final long serialVersionUID = 2825983879781792266L;
+	{
+	  put("Content-Type", "application/rdf+xml");
+	  put("Accept", "application/rdf+xml"); // Both Accept and Content-Type are needed for PubChem 
+	}};
 
 	/**
      * Creates a new IcebearManager.
@@ -28,7 +50,43 @@ public class IcebearManager implements IBactingManager {
      */
 	public IcebearManager(String workspaceRoot) {
 		this.workspaceRoot = workspaceRoot;
+		this.cdk = new CDKManager(this.workspaceRoot);
+		this.rdf = new RDFManager(this.workspaceRoot);
 	}
+
+	public void findInfo(IMolecule mol) throws BioclipseException {
+    	ICDKMolecule cdkMol = cdk.asCDKMolecule(mol);
+    	String inchi = cdkMol.getInChI(Property.USE_CACHED_OR_CALCULATED);
+    	IcebearWorkload workload = new IcebearWorkload();
+    	workload.addNewURI("http://rdf.openmolecules.net/?" + inchi);
+    	inchi = inchi.replace("=1S/", "=1/");
+    	workload.addNewURI("http://rdf.openmolecules.net/?" + inchi);
+    	while (workload.hasMoreWork()) {
+    		findInfoForOneURI(workload);
+    	}
+	}
+
+    private IRDFStore findInfoForOneURI(IcebearWorkload workload) {
+    	IRDFStore store = rdf.createInMemoryStore();
+    	URI nextURI = workload.getNextURI();
+		String nextURIString = nextURI.toString();
+    	try {
+			rdf.addObjectProperty(store,
+				"http://www.bioclipse.org/PrimaryObject", "http://www.bioclipse.org/hasURI",
+				nextURI.toString()
+			);
+			rdf.importURL(store, nextURIString, extraHeaders);
+			System.out.println(rdf.asTurtle(store));
+//			for (INextURIExtractor spider : spiders) {
+//				for (String uri : spider.extractURIs(store, nextURI.toString())) {
+//					workload.addNewURI(uri);
+//				}
+//			}
+		} catch (Exception exception) {
+			System.out.println("Error while downloading " + nextURIString + ": " + exception.getMessage());
+		}
+    	return store;
+    }
 
 	@Override
 	public String getManagerName() {
@@ -38,5 +96,44 @@ public class IcebearManager implements IBactingManager {
 	public List<String> doi() {
 		List<String> dois = new ArrayList<String>();
 		return dois;
+	}
+
+	class IcebearWorkload {
+		
+		Set<URI> todo = new HashSet<URI>();
+		Set<URI> done = new HashSet<URI>();
+
+		public boolean hasMoreWork() {
+			System.out.println("work left todo: " + todo.size());
+			return todo.size() != 0;
+		}
+
+		public URI getNextURI() {
+			URI nextURI = todo.iterator().next();
+			System.out.println("next URI: " + nextURI);
+			todo.remove(nextURI);
+			done.add(nextURI);
+			return nextURI;
+		}
+
+		/**
+		 * Returns false when the URI was already processed or is already scheduled.
+		 */
+		public boolean addNewURI(String newURI) {
+			System.out.println("Adding URI: " + newURI);
+			try {
+				URI uri = new URI(newURI);
+				if (done.contains(uri) || todo.contains(uri)) {
+					System.out.println("Already got it...");
+					return false;
+				}
+
+				todo.add(uri);
+				return true;
+			} catch (URISyntaxException e) {
+				System.out.println("Failed to add the new URI: " + e.getMessage());
+				return false;
+			}
+		}
 	}
 }
